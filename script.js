1ffdca429f04db260ca7f9d689b3a86a36171f2a
ugurduyar <ismailugurduyar@gmail.com>
2020-06-07 07:58:00 +0300

    init function added with transactions

--- script.js
+++ script.js
@@ -17,9 +17,31 @@ let transactions = dummyTransactions;
 
 // Add transactions to DOM list
 
-function addTransactionDOM(transactions) {
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
   // Get sign
   const sign = transaction.amount < 0 ? "-" : "+";
 
   const item = document.createElement("li");
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+      ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+    `;
+
+  list.appendChild(item);
 }
+
+// Init app
+function init() {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionDOM);
+}
+
+init();
